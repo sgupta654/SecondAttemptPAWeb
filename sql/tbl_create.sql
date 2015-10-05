c3ea8d0a016bd57c82a3bc1dd70237ee0af1f890
bliminate <blimmer@umich.edu>
2015-10-04 20:31:03 -0400

    adjustments to test locally

--- sql/tbl_create.sql
+++ sql/tbl_create.sql
@@ -1,4 +1,4 @@
-USE group36;
+USE group36pa2;
 CREATE TABLE User (username VARCHAR(20) NOT NULL, firstname VARCHAR(20), lastname VARCHAR(20), password VARCHAR(20), email VARCHAR(40), Primary Key(username));
 CREATE TABLE Album (albumid INT UNSIGNED NOT NULL AUTO_INCREMENT, title VARCHAR(50), created DATE NOT NULL, lastupdated DATE NOT NULL, username VARCHAR(20) NOT NULL, access VARCHAR(10) NOT NULL, Primary Key(albumid), CONSTRAINT fk_username Foreign Key(username) REFERENCES User(username) ON UPDATE CASCADE ON DELETE CASCADE);
 CREATE TABLE Photo (picid VARCHAR(40) NOT NULL, url VARCHAR(255) NOT NULL, format CHARACTER(3), date DATE NOT NULL, Primary Key(picid));
